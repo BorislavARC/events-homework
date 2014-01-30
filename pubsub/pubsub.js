501798dfb890ac75b45658dc230cd7881f4322fa
boris <Borislav.arc@gmail.com>
2014-01-30 20:20:17 +0200

    vtoroe zadanie

--- pubsub/pubsub.js
+++ pubsub/pubsub.js
@@ -1,66 +1,82 @@
-/**
- * Конструктор класса обмена сообщениями
- * @constructor
- */
+
 function PubSub(){
-};
+    this.handlers = {};
+}
 
-/**
- * Функция подписки на событие
- * @param  {string} eventName имя события
- * @param  {function} handler функция которая будет вызвана при возникновении события
- * @return {function}         ссылка на handler
- */
+
 PubSub.prototype.subscribe = function(eventName, handler) {
-    return handler;
+    if(handler === undefined) {
+        alert('Не ввели функцию, которая будет вызвана при возникновении события');
+        return false;
+    }
+    if (this.handlers[eventName] === undefined) {
+        this.handlers[eventName] = [];
+    }
+    for (var b = 0; b < this.handlers[eventName].length; b++) {
+        if (this.handlers[eventName][b] === handler) {
+            alert('Уже есть такая функция в этом событии');
+            return false;
+        }
+    }
+
+    return this.handlers[eventName].push(handler);
 };
 
-/**
- * Функция отписки от события
- * @param  {string} eventName имя события
- * @param  {function} handler функция которая будет отписана
- * @return {function}         ссылка на handler
- */
+
 PubSub.prototype.unsubscribe = function(eventName, handler) {
-    return handler;
+    if(handler === undefined) {
+        alert('Не ввели функцию, которая будет отписана');
+        return false;
+    }
+    for (var i = 0; i < this.handlers[eventName].length; i++) {
+        if (this.handlers[eventName][i] === handler) {
+            console.log('Функция отписана');
+            return this.handlers[eventName].splice(i, 1);
+        }
+    }
 };
 
-/**
- * Функция генерирующая событие
- * @param  {string} eventName имя события
- * @param  {object} data      данные для обработки соответствующими функциями
- * @return {bool}             удачен ли результат операции
- */
+
 PubSub.prototype.publish = function(eventName, data) {
-    return false;
+    if (this.handlers[eventName] === undefined) {
+        return false;
+    }
+
+    for (var i = 0; i < this.handlers[eventName].length; i++) {
+        this.handlers[eventName][i](arguments[i+1]);
+    }
+    return true;
 };
 
-/**
- * Функция отписывающая все функции от определённого события
- * @param  {string} eventName имя события
- * @return {bool}             удачен ли результат операции
- */
+
 PubSub.prototype.off = function(eventName) {
-    return false;
+    this.handlers[eventName] =  [];
+    return true;
+
 };
 
-/**
- * @example
- *
- * PubSub.subscribe('click', function(event, data) { console.log(data) });
- * var second = PubSub.subscribe('click', function(event, data) { console.log(data) });
- *
- * //Отписать одну функцию от события 'click':
- * PubSub.unsubscribe('click', second);
- *
- * //Отписать группу функций от события 'click'
- * PubSub.off('click');
- */
 
 /*
     Дополнительный вариант — без явного использования глобального объекта
     нужно заставить работать методы верно у любой функции
  */
+
+
+Function.prototype.handlers = {};
+
+Function.prototype.subscribe = function(eventName) {
+    if (Function.prototype.handlers[eventName] === undefined) {
+        Function.prototype.handlers[eventName] = [];
+    }
+    return Function.handlers[eventName].push(this);
+};
+
+Function.prototype.unsubscribe = function(eventName) {
+    if (Function.prototype.handlers[eventName] !== undefined)
+        Function.prototype.handlers[eventName].splice(Function.prototype.handlers[eventName].indexOf(this),1);
+    return true;
+};
+
 
 function foo(event, data) {
     //body…
